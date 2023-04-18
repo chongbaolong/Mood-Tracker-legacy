b82186ab740c5c9326dd87b1894687cebbb77839
xEvl <yf828w@gmail.com>
2023-04-18 22:13:35 +0800

    modify Diary.java and DiaryDBHelper.java delete DiaryContract.java

--- app/src/main/java/com/example/moodtracker/DiaryDBHelper.java
+++ app/src/main/java/com/example/moodtracker/DiaryDBHelper.java
@@ -6,29 +6,41 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DiaryDBHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "diary.db";
+    private static final String DATABASE_NAME = "diaryDB.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + Diary.DiaryEntry.TABLE_NAME + " (" +
+                    Diary.DiaryEntry._ID + " INTEGER PRIMARY KEY," +
+                    Diary.DiaryEntry.COLUMN_SELECTED_DATE + " TEXT NOT NULL, " +
+                    Diary.DiaryEntry.COLUMN_SELECTED_EMOJI + " TEXT NOT NULL, " +
+                    Diary.DiaryEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                    Diary.DiaryEntry.COLUMN_CONTENT + " TEXT NOT NULL)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + Diary.DiaryEntry.TABLE_NAME;
+
+
     public DiaryDBHelper(Context context) {
+
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create the diary table
-        String SQL_CREATE_DIARY_TABLE = "CREATE TABLE " + DiaryContract.DiaryEntry.TABLE_NAME + " ("
-                + DiaryContract.DiaryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + DiaryContract.DiaryEntry.COLUMN_SELECTED_DATE + " TEXT NOT NULL, "
-                + DiaryContract.DiaryEntry.COLUMN_SELECTED_EMOJI + " TEXT NOT NULL, "
-                + DiaryContract.DiaryEntry.COLUMN_TITLE + " TEXT NOT NULL, "
-                + DiaryContract.DiaryEntry.COLUMN_CONTENT + " TEXT NOT NULL);";
-        db.execSQL(SQL_CREATE_DIARY_TABLE);
+
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String SQL_DELETE_DIARY_TABLE = "DROP TABLE IF EXISTS " + DiaryContract.DiaryEntry.TABLE_NAME;
-        db.execSQL(SQL_DELETE_DIARY_TABLE);
+
+        db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
 }
