fbe0817d44f7d4bbd7f2a6e7ce9ddbce53c9e34b
xEvl <yf828w@gmail.com>
2023-04-19 00:01:24 +0800

    modify

--- app/src/main/java/com/example/moodtracker/mainPage.java
+++ app/src/main/java/com/example/moodtracker/mainPage.java
@@ -268,11 +268,11 @@ public class mainPage extends AppCompatActivity {
                     return true;
 
                 case R.id.summary:
+                    mp.start();
                     Intent intentDiary = new Intent(mainPage.this, SummaryPage.class);
                     startActivity(intentDiary);
                     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     return true;
-//                case R.id.what:
 
                 default:
                     return false;
